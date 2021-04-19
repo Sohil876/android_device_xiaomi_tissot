d8d54e051ac6aa4c1ac1cb11be6f46ad22bda28e
Sohil876 <sohil876@gmail.com>
2021-04-19 17:18:37 +0000

    Disable some debuggings

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -38,6 +38,12 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 IS_PHONE := true
 
+# Disable some debuggings
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_PACKAGES_DEBUG := false
+PRODUCT_PACKAGES_DEBUG_ASAN := false
+
 # Build fingerprint
 BUILD_FINGERPRINT := xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys
 BUILD_DESC := tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys
